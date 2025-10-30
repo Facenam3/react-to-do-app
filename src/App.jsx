837ccc415a2bb6abd4d01156846c648ee123f39e
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-10-30 22:26:27 +0100

    added react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-center font-bold text-6xl text-green-600'>Dalibor</h1>
+    </>
+  )
+}
+
+export default App
