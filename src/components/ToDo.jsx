3c5f6640b33c31d0c357c782aec764d9e1eca8cc
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-11-01 14:48:27 +0100

    cleared main

--- src/components/ToDo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Todo() {
-    return (
-        <div className="flex justify-center items-center mt-16">
-            <h1 className="text-5xl font-bold text-gray-900">To Do Application</h1>
-        </div>
-    );
-}
